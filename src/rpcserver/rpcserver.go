e04c90143afa2387f3a46a0267cc631d3dd019ac
Blake Lawson <blakemlaws@gmail.com>
2017-03-11 19:31:18 -0500

    Added tests for RPC server. RPC server and client functions done

--- src/rpcserver/rpcserver.go
+++ src/rpcserver/rpcserver.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
+	"sync"
 )
 
 // RPCServer used to handle client RPC requests.
@@ -73,11 +74,69 @@ func (rpcs *RPCServer) ChordLookup(args *ChordLookupArgs, reply *ChordLookupRepl
 	return nil
 }
 
+// Package level variables to ensure only single server.
+var serverMutex sync.Mutex
+var serverListener net.Listener
+var serverRunning bool
+var serverInitialized bool
+
 // Start RPCServer listening on given port. Does not return until error or
 // program ends. It is an error to call Start more than once.
 func Start(ch *chord.Chord, kv *kvserver.KVServer, port int) error {
-	rpcs := &RPCServer{ch, kv}
-	rpc.Register(rpcs)
-	rpc.HandleHTTP()
-	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	return start(ch, kv, fmt.Sprintf(":%d", port))
+}
+
+// Local start method with more control over address server listens on. Used
+// for testing.
+func start(ch *chord.Chord, kv *kvserver.KVServer, addr string) error {
+	// Ensure only initialized once
+	serverMutex.Lock()
+	if serverRunning {
+		serverMutex.Unlock()
+		return fmt.Errorf("RPCServer: server already running")
+	}
+	serverRunning = true
+	serverMutex.Unlock()
+	defer func() {
+		serverMutex.Lock()
+		serverRunning = false
+		serverMutex.Unlock()
+	}()
+
+	// Set up the RPC handlers.
+	if !serverInitialized {
+		serverInitialized = true
+		rpcs := &RPCServer{ch, kv}
+		rpc.Register(rpcs)
+		rpc.HandleHTTP()
+	}
+
+	var err error
+	serverListener, err = net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	// Run the server.
+	err = http.Serve(serverListener, nil)
+
+	// Ignore acceptable error message (occurs on End()).
+	baseMsg := "accept tcp %s: use of closed network connection"
+	okMsg1 := fmt.Sprintf(baseMsg, fmt.Sprintf("[::]%s", addr))
+	okMsg2 := fmt.Sprintf(baseMsg, addr)
+	if err != nil && err.Error() != okMsg1 && err.Error() != okMsg2 {
+		return err
+	}
+	return nil
+}
+
+// End the server if it is running. Returns nil on success.
+func End() error {
+	serverMutex.Lock()
+	defer serverMutex.Unlock()
+	if !serverRunning {
+		return fmt.Errorf("RPCServer: server not running")
+	}
+
+	return serverListener.Close()
 }
--- src/rpcserver/test_test.go
+++ src/rpcserver/test_test.go
@@ -1,9 +1,98 @@
 // Blake Lawson (blawson@princeton.edu) and Oluwatosin Adewale (oadewale@princeton.edu)
+
 package rpcserver
 
 import (
+	"chord"
 	"fmt"
+	"kvserver"
+	"net"
 	"testing"
+	"time"
+	"util"
 )
 
-// TODO: rpcserver tests go here
+func TestInitialization(t *testing.T) {
+	fmt.Println("Test: RPCServer initialization ...")
+
+	// End server before running should fail.
+	if err := End(); err == nil {
+		t.Fatalf("ended server that wasn't running.")
+	}
+
+	// Create a server
+	port := 8888
+	ch := chord.Make(nil, true)
+	kv := kvserver.Make(ch)
+
+	serverDone := make(chan bool)
+	go func() {
+		if err := Start(ch, kv, port); err != nil {
+			t.Fatalf("server failed: %s", err)
+		}
+		serverDone <- true
+	}()
+
+	// Wait a few seconds to be sure that the server is running.
+	time.Sleep(time.Second)
+
+	// Try creating a new server while the first one is running
+	testWorked := make(chan bool)
+	go func() {
+		if err := Start(ch, kv, port); err == nil {
+			t.Fatalf("server started a second time.")
+		}
+		testWorked <- true
+	}()
+
+	select {
+	case <-time.After(time.Second):
+		t.Fatalf("server started a second time.")
+	case <-testWorked:
+		// Do nothing because it worked.
+	}
+
+	// Turn off server.
+	if err := End(); err != nil {
+		t.Fatalf("error stopping server: %s", err)
+	}
+
+	// Ensure server stopped.
+	select {
+	case <-time.After(time.Second):
+		t.Fatalf("server did not stop")
+	case <-serverDone:
+		// Do nothing because it worked.
+	}
+
+	fmt.Println(" ... Passed")
+}
+
+func TestBasicRequest(t *testing.T) {
+	fmt.Println("Test: RPCServer basic request ...")
+
+	addr := "127.0.0.1"
+	port := 8888
+	ch := chord.Make(nil, true)
+	kv := kvserver.Make(ch)
+
+	go func() {
+		if err := start(ch, kv, fmt.Sprintf("%s:%d", addr, port)); err != nil {
+			t.Fatalf("server failed: %s", err)
+		}
+	}()
+
+	// Let server start.
+	time.Sleep(time.Second)
+
+	// Try connecting to the server.
+	n := util.MakeNode(net.ParseIP(addr), port)
+	if _, err := RemoteGet(n, ""); err != nil {
+		t.Fatalf("RPC failed")
+	}
+
+	// Stop the server.
+	End()
+
+	fmt.Println(" ... Passed")
+}
