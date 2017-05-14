60c2cd7c1abe5bd340316a1716fb74802f4826e0
Blake Lawson <blakemlaws@gmail.com>
2017-05-14 09:39:56 -0400

    Created package for getting own IP address

--- src/chordkv/chordkv.go
+++ src/chordkv/chordkv.go
@@ -3,51 +3,12 @@
 package chordkv
 
 import (
-	"errors"
+	"extip"
 	"fmt"
 	"net"
 	"strconv"
 	"strings"
 )
-
-// externalIP looks up this machine's IP address. This method from
-// http://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
-func externalIP() (string, error) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		return "", err
-	}
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			continue // interface down
-		}
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue // loopback interface
-		}
-		addrs, err := iface.Addrs()
-		if err != nil {
-			return "", err
-		}
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue // not an ipv4 address
-			}
-			return ip.String(), nil
-		}
-	}
-	return "", errors.New("are you connected to the network?")
-}
 
 // ChordKV contains everything everything necessary to use the chord lookup
 // service with a distributed key value store.
@@ -74,7 +35,7 @@ func MakeChordKVDbg(existingNode *Node, useLocalhost bool) (*ChordKV, error) {
 
 	// Get the IP at the start because there is no point continuing if this fails.
 	DPrintf("MakeChordKVDbg: getting externalIP")
-	ip, err := externalIP()
+	ip, err := extip.ExternalIP()
 	if err != nil {
 		return nil, fmt.Errorf("Could not find IP: %s", err)
 	}
---
+++ src/extip/extip.go
@@ -0,0 +1,48 @@
+// Blake Lawson (blawson@princeton.edu) and Oluwatosin Adewale (oadewale@princeton.edu)
+
+package extip
+
+import (
+	"errors"
+	"net"
+)
+
+// externalIP looks up this machine's IP address. This method from
+// http://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
+func ExternalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
+}
+
